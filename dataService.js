c45c3b9348d81c5d2225eda0ceb8cc2666819a4d
Mikko Rinta-Homi <mikkojrh@gmail.com>
2019-02-11 18:59:37 +0200

    Added some csv parsing functionality, added ESLint and fixed errors

--- dataService.js
+++ dataService.js
@@ -1,40 +1,61 @@
 const Papa = require('papaparse')
 const StreamZip = require('node-stream-zip');
-var download = require('download-file')
+const download = require('download-file')
 
-var jsonData = {}
+const jsonData = {}
 
-exports.process = function (dataSource, name) {
-    // ZIP DOWNLOAD
-    const fileName = name.concat(".zip")
-    download(dataSource, { filename: fileName }, function(err){
-        if (err) throw err
-        console.log(`${fileName} downloaded!`)
-        // ZIP EXTRACTION
-        const zip = new StreamZip({
-            file: fileName,
-            storeEntries: true
-        });
-        zip.on('error', err => {
-            `ERROR: error when extracting the data: ${console.log(err)}`
-        })
-        zip.on('ready', () => {
-            const dataFileNames = Object.values(zip.entries()).filter(file => !file.name.includes("Metadata"))
-            if ((dataFileNames.length === 0)) {
-                console.log(`ERROR: no proper files found.`)
-                return this
-            } else if (dataFileNames.length > 1) {
-                console.log(`WARN: found ${dataFileNames.length} matching data files. Using the one ${dataFileNames[0].name}`)
-            }
-            // CSV PARSE
-            console.log(`Converting file ${dataFileNames[0].name}`)
-            let rawData = zip.entryDataSync(dataFileNames[0].name).toString('utf8')
-            rawData = rawData.split('\r\n').slice(4).join(`\r\n`) // Remove first unnecessary lines so csv parser can use the first line as header
-            jsonData[name] = Papa.parse(rawData, { header: true, dynamicTyping: true , skipEmptyLines: true }).data
-            console.log(`${dataFileNames[0].name} convertion done!`)
-        });
+exports.process = (dataSource, name) => {
+  // ### ZIP DOWNLOAD ###
+  const fileName = name.concat('.zip')
+  download(dataSource, { filename: fileName }, (dErr) => {
+    if (dErr) {
+      console.log(`ERROR: cannot download data ${name}: ${dErr}`)
+      return false // Comment this out in order to use pre-loaded data
+    }
+    console.log(`${fileName} downloaded!`)
+    // ### ZIP EXTRACTION ###
+    const zip = new StreamZip({
+      file: fileName,
+      storeEntries: true,
+    });
+    zip.on('error', (zErr) => {
+      console.log(`ERROR: error when extracting the data: ${console.log(zErr)}`)
+      return false
     })
-    return this
+    zip.on('ready', () => {
+      const dataFileNames = Object.values(zip.entries()).filter(file => !file.name.includes("Metadata"))
+      if ((dataFileNames.length === 0)) {
+        console.log('ERROR: no proper files found.')
+        return false
+      } if (dataFileNames.length > 1) {
+        console.log(`WARN: found ${dataFileNames.length} matching data files. Using the one ${dataFileNames[0].name}`)
+      }
+      // ### CSV PARSE ###
+      console.log(`Converting file ${dataFileNames[0].name}`)
+      let rawData = zip.entryDataSync(dataFileNames[0].name).toString('utf8')
+      rawData = rawData.split('\r\n').slice(4).join('\r\n') // Remove first unnecessary lines so csv parser can use the first line as header
+      jsonData[name] = Papa.parse(rawData, { header: true, dynamicTyping: true, skipEmptyLines: true }).data
+      const header = Object.keys(jsonData[name][0])
+      header.pop() // there is always last empty column in this dataset.
+      const startingYear = header.reduce((acc, cur) => (Number.isNaN(Number(cur)) ? acc : Math.min(acc, cur)))
+      const endingYear = header.reduce((acc, cur) => (Number.isNaN(Number(cur)) ? acc : Math.max(acc, cur)))
+      for (const key of Object.values(jsonData[name])) {
+        delete key[''] // Delete the empty element
+        const values = []
+        for (let year = startingYear; year <= endingYear; year++) {
+          values.push(key[year])
+        }
+        key.array = {
+          startingYear: startingYear,
+          endingYear: endingYear,
+          values: values,
+        }
+      }
+      console.log(`${dataFileNames[0].name} convertion done!`)
+      return true
+    });
+    return true
+  })
 }
 
 exports.data = jsonData
--- index.js
+++ index.js
@@ -1,36 +1,37 @@
-const populationDataSource = "http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv"
-const carbondioxideSource = "http://api.worldbank.org/v2/en/indicator/EN.ATM.CO2E.KT?downloadformat=csv"
-const population = "population"
-const carbondioxide = "carbondioxide"
-
-const dataService = require('./dataService')
+/* eslint-disable no-unused-vars */
+const populationDataSource = 'http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv'
+const carbondioxideSource = 'http://api.worldbank.org/v2/en/indicator/EN.ATM.CO2E.KT?downloadformat=csv'
+const population = 'population'
+const carbondioxide = 'carbondioxide'
 const express = require('express')
+const dataService = require('./dataService')
+
 const app = express()
 
 dataService.process(populationDataSource, population)
 dataService.process(carbondioxideSource, carbondioxide)
 
 app.get('/api/population', (req, res, next) => {
-    res.json(dataService.data[population])
+  res.json(dataService.data[population])
 })
 
 app.get('/api/population/:country', (req, res, next) => {
-    const countryName = req.params.country
-    countryData = dataService.data[population].filter(country => country["Country Name"].toUpperCase().includes(countryName.toUpperCase()))
-    res.json(countryData)
+  const countryName = req.params.country
+  const countryData = dataService.data[population].filter(country => country['Country Name'].toUpperCase().includes(countryName.toUpperCase()))
+  res.json(countryData)
 })
 
 app.get('/api/carbondioxide', (req, res, next) => {
-    res.json(dataService.data[carbondioxide])
+  res.json(dataService.data[carbondioxide])
 })
 
 app.get('/api/carbondioxide/:country', (req, res, next) => {
-    const countryName = req.params.country
-    countryData = dataService.data[carbondioxide].filter(country => country["Country Name"].toUpperCase().includes(countryName.toUpperCase()))
-    res.json(countryData)
+  const countryName = req.params.country
+  const countryData = dataService.data[carbondioxide].filter(country => country['Country Name'].toUpperCase().includes(countryName.toUpperCase()))
+  res.json(countryData)
 })
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
