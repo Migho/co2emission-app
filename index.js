f3142b9d221a3f711b685846318f79d2dcaaff54
Mikko Rinta-Homi <mikkojrh@gmail.com>
2019-02-19 17:12:40 +0200

    Scheduled data updating, minor changes

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const populationDataSource = 'http://api.worldbank.org/v2/en/indicator/SP.POP.TO
 const carbondioxideSource = 'http://api.worldbank.org/v2/en/indicator/EN.ATM.CO2E.KT?downloadformat=csv'
 const population = 'population'
 const carbondioxide = 'carbondioxide'
+const cors = require('cors')
 const express = require('express')
+const schedule = require('node-schedule')
 const dataService = require('./dataService')
 
 const app = express()
+app.use(cors())
 
-dataService.process(populationDataSource, population)
-dataService.process(carbondioxideSource, carbondioxide)
+async function updateData() {
+  await dataService.process(populationDataSource, population)
+  await dataService.process(carbondioxideSource, carbondioxide)
+}
+
+schedule.scheduleJob('0 4 * * *', () => {
+  updateData()
+});
 
 app.get('/api/population', (req, res, next) => {
   res.json(dataService.data[population])
@@ -31,7 +40,18 @@ app.get('/api/carbondioxide/:country', (req, res, next) => {
   res.json(countryData)
 })
 
-const PORT = process.env.PORT || 3000
+app.get('/api/carbondioxidePerCapita', (req, res, next) => {
+  res.json(dataService.data[carbondioxide])
+})
+
+app.get('/api/carbondioxidePerCapita/:country', (req, res, next) => {
+  const countryName = req.params.country
+  const countryData = dataService.data[carbondioxide].filter(country => country['Country Name'].toUpperCase().includes(countryName.toUpperCase()))
+  res.json(countryData)
+})
+
+updateData()
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
